912f331a1b269a4c1eea2c59ea7e4c7c5e206509
michas <michas@jarmoni.org>
2014-09-23 21:43:51 +0200

    added toString()

--- src/main/java/org/jarmoni/resource/Repository.java
+++ src/main/java/org/jarmoni/resource/Repository.java
@@ -1,5 +1,7 @@
 package org.jarmoni.resource;
 
+import com.google.common.base.MoreObjects;
+
 public class Repository {
 
 	public String name;
@@ -7,4 +9,14 @@ public class Repository {
 	public boolean autoCommit;
 	public boolean autoPush;
 	public boolean autoPull;
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return MoreObjects.toStringHelper(Repository.class)
+				.add("name", this.name).add("path", this.path)
+				.add("autoCommit", this.autoCommit)
+				.add("autoPush", this.autoPush).add("autoPull", this.autoPull)
+				.toString();
+	}
 }
