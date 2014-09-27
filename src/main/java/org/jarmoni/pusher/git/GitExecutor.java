4853ae9c2b351c938d192b2bfbc8fb9ab4baa6b5
michas <michas@jarmoni.org>
2014-09-28 00:00:28 +0200

    #createRepository() is void-method

--- src/main/java/org/jarmoni/pusher/git/GitExecutor.java
+++ src/main/java/org/jarmoni/pusher/git/GitExecutor.java
@@ -13,21 +13,21 @@ public class GitExecutor {
 
 	private static final Logger LOG = LoggerFactory.getLogger(GitExecutor.class);
 
-	public Repository createRepository(final Path directory) {
+	public void createRepository(final Path directory) {
 		// does directory exist at all?
 		if (!Files.isDirectory(directory)) {
 			if (!directory.toFile().mkdirs()) {
 				throw new RuntimeException("Could not create directory=" + directory);
 			}
 			LOG.info("Will create GIT-repository in newly created directory");
-			return this.createRepositoryInternal(directory);
+			this.createRepositoryInternal(directory);
 		}
 		else {
 			final Path gitDir = directory.resolve(GIT_DIR_NAME);
 			// does the (existing) directory contain a '.git'-dir?
 			if (!Files.isDirectory(gitDir)) {
 				LOG.info("Will create GIT-repository in existing directory");
-				return this.createRepositoryInternal(directory);
+				this.createRepositoryInternal(directory);
 			}
 			else {
 				throw new RuntimeException("Directory already contains a '.git'-folder");
