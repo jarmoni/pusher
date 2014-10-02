8d4cccadf0ae791843b15a594ace327ff33ba22d
michas <michas@jarmoni.org>
2014-10-02 12:07:13 +0200

    use map instead of list

--- src/main/java/org/jarmoni/pusher/service/PusherService.java
+++ src/main/java/org/jarmoni/pusher/service/PusherService.java
@@ -11,12 +11,13 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.eclipse.jgit.lib.Repository;
+import org.jarmoni.pusher.git.GitExecutor;
 import org.jarmoni.resource.RepositoryResource;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Throwables;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class PusherService implements IPusherService {
@@ -26,10 +27,12 @@ public class PusherService implements IPusherService {
 	private Path appHome;
 	private final Path reposFile;
 
-	private Map<String, Pair<Repository, RepositoryResource>> repositories2 = Maps.newHashMap();
-	private List<RepositoryResource> repositories = Lists.newArrayList();
+	private Map<String, Pair<Repository, RepositoryResource>> repositories = Maps.newHashMap();
 
-	public PusherService(final String appHome) {
+	private GitExecutor gitExecutor;
+
+	public PusherService(final String appHome, GitExecutor gitExecutor) {
+		this.gitExecutor = Preconditions.checkNotNull(gitExecutor);
 		final Path path = Paths.get(Preconditions.checkNotNull(appHome));
 		if (Files.isDirectory(path)) {
 			this.appHome = path;
@@ -45,12 +48,12 @@ public class PusherService implements IPusherService {
 		this.reposFile = this.appHome.resolve(REPOS_FILE_NAME);
 	}
 
-	@SuppressWarnings("unchecked")
 	void reloadRepositories() {
 		if (Files.isRegularFile(this.reposFile)) {
 			try (final InputStream is = Files.newInputStream(this.reposFile)) {
 				final ObjectMapper mapper = new ObjectMapper();
-				this.repositories = mapper.readValue(is, List.class);
+				List<RepositoryResource> reposList = mapper.readValue(is, new TypeReference<List<RepositoryResource>>(){});
+				reposList.forEach(res -> this.repositories.put(res.getName(), Pair.of(this.gitExecutor.openRepository(Paths.get(res.getPath(), GitExecutor.GIT_DIR_NAME)), res)));
 			}
 			catch (final Throwable t) {
 				Throwables.propagate(t);
@@ -61,7 +64,7 @@ public class PusherService implements IPusherService {
 	void saveRepositories() {
 		try (OutputStream os = Files.newOutputStream(this.reposFile)) {
 			final ObjectMapper mapper = new ObjectMapper();
-			mapper.writeValue(os, this.repositories);
+			mapper.writeValue(os, this.getRepositories());
 		}
 		catch (final Throwable t) {
 			Throwables.propagate(t);
@@ -74,38 +77,47 @@ public class PusherService implements IPusherService {
 
 	@Override
 	public List<RepositoryResource> getRepositories() {
-		return this.repositories;
+		return this.repositories.entrySet().stream().map(e -> e.getValue().getRight()).collect(Collectors.toList());
 	}
 
 	@Override
 	public RepositoryResource getRepository(final String name) {
-		return this.repositories.stream().filter(rep -> name.equals(rep.getName()))
-				.findFirst().orElseThrow(() -> new RuntimeException("Repository does not exist=" + name));
+		return this.checkReposExist(name, true).getRight();
 	}
 
 	@Override
 	public RepositoryResource createRepository(final RepositoryResource repository) {
-		this.repositories.add(repository);
+		this.checkReposExist(repository.getName(), false);
+		this.repositories.put(repository.getName(),
+				Pair.of(this.gitExecutor.openAndCreateRepository(Paths.get(repository.getPath())), repository));
 		return repository;
 	}
 
 	@Override
 	public void deleteRepository(final String name) {
-		this.repositories = this.repositories.stream().filter(rep -> !rep.getName().equals(name))
-		.collect(Collectors.toList());
+		this.checkReposExist(name, true);
+		this.repositories.remove(name);
 	}
 
 	@Override
 	public RepositoryResource updateRepository(final RepositoryResource repository) {
-		this.repositories = this.repositories.stream()
-		.filter(rep -> !rep.getName().equals(repository.getName()))
-		.collect(Collectors.toList());
-		this.repositories.add(repository);
+		this.repositories.put(repository.getName(),
+				Pair.of(this.checkReposExist(repository.getName(), true).getLeft(), repository));
 		return repository;
 	}
 
 	@Override
 	public void triggerRepository(final String name) {
 		// TODO Do the trigger-stuff here.....
+	}
+
+	private Pair<Repository, RepositoryResource> checkReposExist(String name, boolean mustExist) {
+		if (mustExist) {
+			return Preconditions.checkNotNull(this.repositories.get(name), "Repository does not exist. Repository=" + name);
+		}
+		else {
+			Preconditions.checkState(this.repositories.get(name) == null, "Repository does already exist. Repository=" + name);
+			return null;
+		}
 	}
 }
