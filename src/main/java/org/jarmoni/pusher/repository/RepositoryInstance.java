0139cf2c7842f3025449695edf6d1ff02c566aa3
michas <michas@jarmoni.org>
2015-03-23 22:52:43 +0100

    some logging...

--- src/main/java/org/jarmoni/pusher/repository/RepositoryInstance.java
+++ src/main/java/org/jarmoni/pusher/repository/RepositoryInstance.java
@@ -9,10 +9,14 @@ import org.jarmoni.pusher.file.FileChangeScanner;
 import org.jarmoni.pusher.file.IFileChangeListener;
 import org.jarmoni.pusher.git.GitExecutor;
 import org.jarmoni.pusher.resource.RepositoryResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 
 public class RepositoryInstance implements IFileChangeListener {
+
+	private static final Logger LOG = LoggerFactory.getLogger(RepositoryInstance.class);
 
 	private final Repository repository;
 	private final RepositoryResource repositoryResource;
@@ -28,14 +32,21 @@ public class RepositoryInstance implements IFileChangeListener {
 	}
 
 	public void start() {
+		LOG.info("Trying to start RepositoryInstance={}", repositoryResource.getName());
 		this.fileChangeScanner = new FileChangeScanner(this, Paths.get(repositoryResource.getPath()));
 		this.fileChangeScanner.start();
+		LOG.info("RepositoryInstance started={}", repositoryResource.getName());
 	}
 
 	public void stop() {
 		if (this.fileChangeScanner != null) {
+			LOG.info("Trying to stop RepositoryInstance={}", repositoryResource.getName());
 			this.fileChangeScanner.stop();
 			this.fileChangeScanner = null;
+			LOG.info("RepositoryInstance stopped={}", repositoryResource.getName());
+		}
+		else {
+			LOG.info("RepositoryInstance not running={}", repositoryResource.getName());
 		}
 	}
 
