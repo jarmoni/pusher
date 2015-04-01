c9006385b318c9ea23fc306a9f0919a3f7ee5c27
michas <michas@jarmoni.org>
2015-04-02 00:24:19 +0200

    improved binding

--- src/main/resources/public/js/main/PusherService.js
+++ src/main/resources/public/js/main/PusherService.js
@@ -3,14 +3,15 @@
     app.factory('PusherService', function ($http) {
         var PusherService = {};
 
-        var repositories = [{"name" : "repos1", "path" : "/var/log/xyz", "autoCommit" : true, "autoSync" : false}, {"name" : "repos2", "path" : "/home/xyz/bla", "autoCommit" : true, "autoSync" : true}, {"name" : "repos3", "path" : "/root/temp", "autoCommit" : false, "autoSync" : false}];
+
 
         PusherService.listRepositories = function() {
+            var repositories = [{"name" : "repos1", "path" : "/var/log/xyz", "autoCommit" : true, "autoSync" : false}, {"name" : "repos2", "path" : "/home/xyz/bla", "autoCommit" : true, "autoSync" : true}, {"name" : "repos3", "path" : "/root/temp", "autoCommit" : false, "autoSync" : false}];
 			return repositories;
 		};
 
         PusherService.getRepository = function(name) {
-            return _.find(repositories, function(repos) {
+            return _.find(PusherService.listRepositories(), function(repos) {
                 return repos.name === name;
             });
         }
--- src/main/resources/public/js/repository/RepositoryController.js
+++ src/main/resources/public/js/repository/RepositoryController.js
@@ -4,14 +4,24 @@
     app.controller("RepositoryController", function ($scope, $state, $stateParams, PusherService) {
 
         var repository = PusherService.getRepository($stateParams["repositoryName"]);
-        $scope.repository = repository;
-        //$scope.repositoryName = repository.name;
-        //$scope.repositoryPath = repository.path;
 
-        $scope.formModified = function(name, path, autoCommit, autoSync) {
+        $scope.repository = repository;
+        $scope.reposOrigName = repository.name;
+
+        $scope.formModified = function() {
             var repository = PusherService.getRepository($stateParams["repositoryName"]);
-            var repos2 = { name: name, path: path, autoCommit: autoCommit, autoSync: autoSync};
-            return !_.isEqual(repository, repos2);
+            var repos2 = { name: $scope.repository.name, path: $scope.repository.path, autoCommit: $scope.repository.autoCommit, autoSync: $scope.repository.autoSync};
+            var modified = !_.isEqual(repository, repos2);
+            return modified;
+        }
+
+        $scope.reposName = function() {
+            return $scope.repository.name;
+        }
+
+        // just for debugging:
+        $scope.currentRepos = function() {
+            return $scope.repository;
         }
 
     });
