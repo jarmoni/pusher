b2556569c230304856ec49167ca9b944dfe40c1c
michas <michas@jarmoni.org>
2015-04-02 20:33:43 +0200

    mark dirty when 'name' of repos has changed

--- src/main/resources/public/js/repository/RepositoryController.js
+++ src/main/resources/public/js/repository/RepositoryController.js
@@ -5,13 +5,25 @@
 
         var repository = PusherService.getRepository($stateParams["repositoryName"]);
 
+        //var repository = null;
+        //var reposOrigName = "New Repository";
+        //var currentReposName = $stateParams["repositoryName"];
+        //
+        //if(currentReposName) {
+        //    repository = PusherService.getRepository($stateParams["repositoryName"]);
+        //    reposOrigName = repository.name;
+        //}
+
         $scope.repository = repository;
         $scope.reposOrigName = repository.name;
 
         $scope.formModified = function() {
-            var repository = PusherService.getRepository($stateParams["repositoryName"]);
+            var repository = PusherService.getRepository($scope.repository.name);
             var repos2 = { name: $scope.repository.name, path: $scope.repository.path, autoCommit: $scope.repository.autoCommit, autoSync: $scope.repository.autoSync};
+            console.log("repository=" + JSON.stringify(repository));
+            console.log("repos2=" + JSON.stringify(repos2));
             var modified = !_.isEqual(repository, repos2);
+            console.log("modified=" + modified);
             return modified;
         }
 
